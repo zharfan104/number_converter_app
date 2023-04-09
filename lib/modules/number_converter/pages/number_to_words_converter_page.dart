6e0fdf90ccd7db5bebd959489e6a846cbf970ab5
Zharfan Akbar <zharfan.akbar104@gmail.com>
2023-04-09 20:15:42 +0700

    Add NumberToWordsConverterPage

---
+++ lib/modules/number_converter/pages/number_to_words_converter_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../cubits/number_to_words_cubit.dart';
+import 'widgets/number_to_words_converter_body.dart';
+import 'widgets/number_to_words_converter_drawer.dart';
+
+class NumberToWordsConverterPage extends StatelessWidget {
+  const NumberToWordsConverterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => NumberToWordsCubit(),
+      child: Scaffold(
+        appBar: AppBar(title: const Text('Converter App')),
+        body: const NumberToWordsConverterBody(),
+        drawer: const NumberToWordsConverterDrawer(),
+      ),
+    );
+  }
+}
---
+++ test/modules/number_converter/pages/number_to_words_converter_page_test.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:number_converter_app/modules/number_converter/pages/number_to_words_converter_page.dart';
+import 'package:number_converter_app/modules/number_converter/pages/widgets/number_to_words_converter_body.dart';
+import 'package:number_converter_app/modules/number_converter/pages/widgets/number_to_words_converter_drawer.dart';
+
+void main() {
+  testWidgets(
+    'NumberToWordsConverterPage has app bar, body, and drawer',
+    (WidgetTester tester) async {
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: NumberToWordsConverterPage(),
+        ),
+      );
+
+      // Verify if AppBar is found
+      expect(find.byType(AppBar), findsOneWidget);
+
+      // Verify if the AppBar title text is "Converter App"
+      expect(find.text('Converter App'), findsOneWidget);
+
+      // Verify if NumberToWordsConverterBody is found
+      expect(find.byType(NumberToWordsConverterBody), findsOneWidget);
+
+      // Open the drawer by tapping the hamburger icon
+      await tester.tap(find.byIcon(Icons.menu));
+      await tester.pumpAndSettle();
+
+      // Verify if NumberToWordsConverterDrawer is found
+      expect(find.byType(NumberToWordsConverterDrawer), findsOneWidget);
+    },
+  );
+}
