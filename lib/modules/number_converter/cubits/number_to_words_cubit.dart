43ebaea8bb387d1480913ea69d4d870d2333d0a0
Zharfan Akbar <zharfan.akbar104@gmail.com>
2023-04-09 19:46:19 +0700

    Add NumberToWordsCubit

---
+++ lib/modules/number_converter/cubits/number_to_words_cubit.dart
@@ -0,0 +1,146 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../../common/utils/string_extension.dart';
+
+import 'number_to_words_state.dart';
+
+const _kMaximumNumberAllowed = 999999999999999;
+
+class NumberToWordsCubit extends Cubit<NumberToWordsState> {
+  NumberToWordsCubit() : super(const NumberToWordsState.initial());
+
+  void updateInputNumber(String inputNumber) {
+    emit(NumberToWordsState.inputChanged(
+      inputNumber: inputNumber,
+      previousOutputWords: state.outputWords,
+    ));
+  }
+
+  void convert() {
+    final inputNumber = state.inputNumber;
+
+    final errorMessage = _inputValidator(inputNumber);
+
+    if (errorMessage == null) {
+      convertNumberToWords(inputNumber);
+    } else {
+      emit(NumberToWordsState.error(
+        errorMessage: errorMessage,
+        previousInputNumber: inputNumber,
+      ));
+    }
+  }
+
+  // Return null if the input is valid
+  String? _inputValidator(String inputNumber) {
+    if (inputNumber.isEmpty) return 'Input is empty';
+    if (inputNumber.length > 1 && (inputNumber.startsWith('0') || inputNumber.startsWith('-0'))) {
+      return 'Input has leading zeros';
+    }
+    if (inputNumber.contains('.')) return 'Input contains a decimal point';
+    if (inputNumber.contains(',')) return 'Input contains a comma';
+
+    final BigInt? parsedNumber = BigInt.tryParse(inputNumber);
+    if (parsedNumber == null) return 'Input is not a valid number';
+
+    if (parsedNumber.abs() > BigInt.from(_kMaximumNumberAllowed)) {
+      return 'Input number exceeds the allowed range';
+    }
+
+    return null;
+  }
+
+  void convertNumberToWords(String inputNumber) {
+    if (inputNumber == '0') {
+      emit(
+        NumberToWordsState.converted(inputNumber: inputNumber, outputWords: 'Zero'),
+      );
+
+      return;
+    }
+
+    final isNegative = inputNumber.startsWith('-');
+    final positiveNumber = isNegative ? inputNumber.substring(1) : inputNumber;
+
+    String tempNumber = positiveNumber;
+    final segments = <String>[];
+    int bigsIndex = 0;
+
+    const bigs = ['', 'thousand', 'million', 'billion', 'trillion'];
+
+    while (tempNumber.isNotEmpty) {
+      final segmentLength = (tempNumber.length > 3) ? 3 : tempNumber.length;
+      final segmentIndex = tempNumber.length - segmentLength;
+      final segment = tempNumber.substring(segmentIndex);
+      tempNumber = tempNumber.substring(0, segmentIndex);
+
+      String segmentText = _convertSegment(segment);
+
+      if (segmentText.isNotEmpty) {
+        if (bigsIndex > 0) {
+          segmentText += ' ${bigs[bigsIndex]}';
+        }
+        segments.insert(0, segmentText);
+      }
+
+      bigsIndex++;
+    }
+
+    String outputWords = segments.join(' ');
+    if (isNegative) {
+      outputWords = 'negative $outputWords';
+    }
+
+    emit(NumberToWordsState.converted(
+      inputNumber: inputNumber,
+      outputWords: outputWords.capitalize(),
+    ));
+  }
+
+  String _convertSegment(String segment) {
+    const units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+
+    const teens = [
+      'ten',
+      'eleven',
+      'twelve',
+      'thirteen',
+      'fourteen',
+      'fifteen',
+      'sixteen',
+      'seventeen',
+      'eighteen',
+      'nineteen',
+    ];
+
+    const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    int num = int.parse(segment);
+    final parts = <String>[];
+    bool isTensIncluded = false;
+
+    if (num >= 100) {
+      parts.add('${units[num ~/ 100]} hundred');
+      num %= 100;
+    }
+
+    if (num >= 10 && num <= 19) {
+      parts.add(teens[num - 10]);
+    } else {
+      if (num >= 20) {
+        parts.add(tens[num ~/ 10]);
+        isTensIncluded = true;
+      }
+      num %= 10;
+      if (num > 0) {
+        if (parts.isNotEmpty) {
+          if (isTensIncluded) {
+            parts.add('-');
+          }
+        }
+        parts.add(units[num]);
+      }
+    }
+
+    return parts.join(' ').replaceAll(' - ', '-').capitalize();
+  }
+}
---
+++ test/modules/number_converter/cubits/number_to_words_cubit_test.dart
@@ -0,0 +1,269 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:number_converter_app/modules/number_converter/cubits/number_to_words_cubit.dart';
+import 'package:number_converter_app/modules/number_converter/cubits/number_to_words_state.dart';
+
+void main() {
+  group('NumberToWordsCubit', () {
+    late NumberToWordsCubit cubit;
+
+    setUp(() {
+      cubit = NumberToWordsCubit();
+    });
+
+    tearDown(() async {
+      await cubit.close();
+    });
+
+    test('initial state is NumberToWordsState.initial()', () {
+      expect(cubit.state, const NumberToWordsState.initial());
+    });
+
+    blocTest<NumberToWordsCubit, NumberToWordsState>(
+      'updates inputNumber and previousOutputWords when updateInputNumber is called',
+      build: () => cubit,
+      act: (cubit) => cubit.updateInputNumber('123'),
+      expect: () => const [
+        NumberToWordsState.inputChanged(inputNumber: '123', previousOutputWords: ''),
+      ],
+    );
+
+    group('convert() emits error state', () {
+      blocTest<NumberToWordsCubit, NumberToWordsState>(
+        'emits error state when input is empty',
+        build: () => cubit,
+        act: (cubit) {
+          cubit.updateInputNumber('');
+          cubit.convert();
+        },
+        expect: () => const [
+          NumberToWordsState.inputChanged(
+            inputNumber: '',
+            previousOutputWords: '',
+          ),
+          NumberToWordsState.error(
+            errorMessage: 'Input is empty',
+            previousInputNumber: '',
+          ),
+        ],
+      );
+
+      blocTest<NumberToWordsCubit, NumberToWordsState>(
+        'emits error state when input has leading zeros (00123)',
+        build: () => cubit,
+        act: (cubit) {
+          cubit.updateInputNumber('00123');
+          cubit.convert();
+        },
+        expect: () => const [
+          NumberToWordsState.inputChanged(inputNumber: '00123', previousOutputWords: ''),
+          NumberToWordsState.error(errorMessage: 'Input has leading zeros', previousInputNumber: '00123'),
+        ],
+      );
+
+      blocTest<NumberToWordsCubit, NumberToWordsState>(
+        'emits error state when input has leading zeros (-00123)',
+        build: () => cubit,
+        act: (cubit) {
+          cubit.updateInputNumber('-00123');
+          cubit.convert();
+        },
+        expect: () => const [
+          NumberToWordsState.inputChanged(inputNumber: '-00123', previousOutputWords: ''),
+          NumberToWordsState.error(errorMessage: 'Input has leading zeros', previousInputNumber: '-00123'),
+        ],
+      );
+
+      blocTest<NumberToWordsCubit, NumberToWordsState>(
+        'emits error state when input contains a decimal point',
+        build: () => cubit,
+        act: (cubit) {
+          cubit.updateInputNumber('123.45');
+          cubit.convert();
+        },
+        expect: () => const [
+          NumberToWordsState.inputChanged(inputNumber: '123.45', previousOutputWords: ''),
+          NumberToWordsState.error(errorMessage: 'Input contains a decimal point', previousInputNumber: '123.45'),
+        ],
+      );
+
+      blocTest<NumberToWordsCubit, NumberToWordsState>(
+        'emits error state when input contains a comma',
+        build: () => cubit,
+        act: (cubit) {
+          cubit.updateInputNumber('123,45');
+          cubit.convert();
+        },
+        expect: () => const [
+          NumberToWordsState.inputChanged(inputNumber: '123,45', previousOutputWords: ''),
+          NumberToWordsState.error(errorMessage: 'Input contains a comma', previousInputNumber: '123,45'),
+        ],
+      );
+
+      blocTest<NumberToWordsCubit, NumberToWordsState>(
+        'emits error state when input is not a valid number',
+        build: () => cubit,
+        act: (cubit) {
+          cubit.updateInputNumber('12a34');
+          cubit.convert();
+        },
+        expect: () => const [
+          NumberToWordsState.inputChanged(inputNumber: '12a34', previousOutputWords: ''),
+          NumberToWordsState.error(errorMessage: 'Input is not a valid number', previousInputNumber: '12a34'),
+        ],
+      );
+
+      blocTest<NumberToWordsCubit, NumberToWordsState>(
+        'emits error state when input number exceeds the maximum allowed value',
+        build: () => cubit,
+        act: (cubit) {
+          cubit.updateInputNumber('1000000000000000');
+          cubit.convert();
+        },
+        expect: () => const [
+          NumberToWordsState.inputChanged(inputNumber: '1000000000000000', previousOutputWords: ''),
+          NumberToWordsState.error(
+            errorMessage: 'Input number exceeds the allowed range',
+            previousInputNumber: '1000000000000000',
+          ),
+        ],
+      );
+
+      blocTest<NumberToWordsCubit, NumberToWordsState>(
+        'emits error state when input number exceeds the minimum allowed value',
+        build: () => cubit,
+        act: (cubit) {
+          cubit.updateInputNumber('-1000000000000000');
+          cubit.convert();
+        },
+        expect: () => const [
+          NumberToWordsState.inputChanged(inputNumber: '-1000000000000000', previousOutputWords: ''),
+          NumberToWordsState.error(
+            errorMessage: 'Input number exceeds the allowed range',
+            previousInputNumber: '-1000000000000000',
+          ),
+        ],
+      );
+
+      blocTest<NumberToWordsCubit, NumberToWordsState>(
+        'emits error state when input contains a space',
+        build: () => cubit,
+        act: (cubit) {
+          cubit.updateInputNumber('12 34');
+          cubit.convert();
+        },
+        expect: () => const [
+          NumberToWordsState.inputChanged(inputNumber: '12 34', previousOutputWords: ''),
+          NumberToWordsState.error(errorMessage: 'Input is not a valid number', previousInputNumber: '12 34'),
+        ],
+      );
+
+      blocTest<NumberToWordsCubit, NumberToWordsState>(
+        'emits error state when input has a negative sign in the middle',
+        build: () => cubit,
+        act: (cubit) {
+          cubit.updateInputNumber('12-34');
+          cubit.convert();
+        },
+        expect: () => const [
+          NumberToWordsState.inputChanged(inputNumber: '12-34', previousOutputWords: ''),
+          NumberToWordsState.error(errorMessage: 'Input is not a valid number', previousInputNumber: '12-34'),
+        ],
+      );
+    });
+
+    group('convert() emits converted state', () {
+      void testWithInputNumber(
+        String inputNumber,
+        String outputWords,
+      ) {
+        blocTest<NumberToWordsCubit, NumberToWordsState>(
+          'emits error when convert is called with input $inputNumber',
+          build: () => cubit,
+          act: (cubit) {
+            cubit.updateInputNumber(inputNumber);
+            cubit.convert();
+          },
+          expect: () => [
+            NumberToWordsState.inputChanged(inputNumber: inputNumber, previousOutputWords: ''),
+            NumberToWordsState.converted(
+              inputNumber: inputNumber,
+              outputWords: outputWords,
+            ),
+          ],
+        );
+      }
+
+      group('Zero Number', () {
+        testWithInputNumber('0', 'Zero');
+      });
+
+      group('Positive Input Number', () {
+        testWithInputNumber('1', 'One');
+        testWithInputNumber('10', 'Ten');
+        testWithInputNumber('11', 'Eleven');
+        testWithInputNumber('12', 'Twelve');
+        testWithInputNumber('13', 'Thirteen');
+        testWithInputNumber('19', 'Nineteen');
+        testWithInputNumber('20', 'Twenty');
+        testWithInputNumber('21', 'Twenty-one');
+        testWithInputNumber('29', 'Twenty-nine');
+        testWithInputNumber('51', 'Fifty-one');
+        testWithInputNumber('59', 'Fifty-nine');
+        testWithInputNumber('89', 'Eighty-nine');
+        testWithInputNumber('100', 'One hundred');
+        testWithInputNumber('101', 'One hundred one');
+        testWithInputNumber('123', 'One hundred twenty-three');
+        testWithInputNumber('111', 'One hundred eleven');
+        testWithInputNumber('1000', 'One thousand');
+        testWithInputNumber('1073', 'One thousand seventy-three');
+        testWithInputNumber('10000', 'Ten thousand');
+        testWithInputNumber('12345', 'Twelve thousand three hundred forty-five');
+        testWithInputNumber('921503', 'Nine hundred twenty-one thousand five hundred three');
+        testWithInputNumber('100000', 'One hundred thousand');
+        testWithInputNumber('1000000', 'One million');
+        testWithInputNumber('1000000000', 'One billion');
+        testWithInputNumber('1000000000000', 'One trillion');
+      });
+
+      group('Negative Input Number', () {
+        testWithInputNumber('-1', 'Negative one');
+        testWithInputNumber('-10', 'Negative ten');
+        testWithInputNumber('-11', 'Negative eleven');
+        testWithInputNumber('-12', 'Negative twelve');
+        testWithInputNumber('-13', 'Negative thirteen');
+        testWithInputNumber('-19', 'Negative nineteen');
+        testWithInputNumber('-20', 'Negative twenty');
+        testWithInputNumber('-21', 'Negative twenty-one');
+        testWithInputNumber('-29', 'Negative twenty-nine');
+        testWithInputNumber('-51', 'Negative fifty-one');
+        testWithInputNumber('-59', 'Negative fifty-nine');
+        testWithInputNumber('-89', 'Negative eighty-nine');
+        testWithInputNumber('-100', 'Negative one hundred');
+        testWithInputNumber('-101', 'Negative one hundred one');
+        testWithInputNumber('-123', 'Negative one hundred twenty-three');
+        testWithInputNumber('-111', 'Negative one hundred eleven');
+        testWithInputNumber('-1000', 'Negative one thousand');
+        testWithInputNumber('-1073', 'Negative one thousand seventy-three');
+        testWithInputNumber('-10000', 'Negative ten thousand');
+        testWithInputNumber('-12345', 'Negative twelve thousand three hundred forty-five');
+        testWithInputNumber('-921503', 'Negative nine hundred twenty-one thousand five hundred three');
+        testWithInputNumber('-100000', 'Negative one hundred thousand');
+        testWithInputNumber('-1000000', 'Negative one million');
+        testWithInputNumber('-1000000000', 'Negative one billion');
+        testWithInputNumber('-1000000000000', 'Negative one trillion');
+      });
+
+      group('Boundary Input Number', () {
+        testWithInputNumber(
+          '999999999999999',
+          'Nine hundred ninety-nine trillion nine hundred ninety-nine billion nine hundred ninety-nine million nine hundred ninety-nine thousand nine hundred ninety-nine',
+        );
+        testWithInputNumber(
+          '-999999999999999',
+          'Negative nine hundred ninety-nine trillion nine hundred ninety-nine billion nine hundred ninety-nine million nine hundred ninety-nine thousand nine hundred ninety-nine',
+        );
+      });
+    });
+  });
+}
